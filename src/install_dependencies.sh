8dc24cd636759b42364a283225505f55bc8538b0
Valentin Bauer <valentin.bauer@tuwien.ac.at>
2024-12-16 16:35:07 +0100

    Update trainging requirements

--- src/install_dependencies.sh
+++ src/install_dependencies.sh
@@ -1,3 +1,3 @@
-pip install numpy pandas scikit-learn scikit-image tqdm matplotlib
+pip install numpy pandas scikit-learn scikit-image tqdm matplotlib tabulate
 
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
